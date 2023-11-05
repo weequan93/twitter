16def20e73441ffbd3684202acd246ecf2290d05
weequan <wee@hyperlegend.onmicrosoft.com>
2023-11-06 02:03:26 +0800

    retweeted_by

--- tweets.go
+++ tweets.go
@@ -260,3 +260,54 @@ func (api *Twitter) GetTweetByID(id string, v url.Values, options ...QueueOption
 	// return the data channel
 	return data, errors
 }
+
+// GetTweetByID returns a variety of information about a single Tweet specified by the requested ID.
+// Endpoint URL: https://api.twitter.com/2/tweets/:id
+// Official Documentation: https://developer.twitter.com/en/docs/twitter-api/tweets/lookup/api-reference//get-tweets-id
+// Authentication Methods: OAuth 1.0a User Context, OAuth 2.0 Bearer Token
+// Rate Limit: 300/15m (app), 900/15m (user)
+func (api *Twitter) GetReTweetedBy(id string, v url.Values, options ...QueueOption) (chan *Data, chan error) {
+	// create the queue to process requests
+	queue := NewQueue(15*time.Minute/1500, 15*time.Minute, true, make(chan *Request), make(chan *Response), options...)
+	// create the temp results channel
+	data := make(chan *Data)
+	errors := make(chan error)
+	// create the request object
+	request, _ := NewRquest("GET", fmt.Sprintf("%s/tweets/%s/retweeted_by", api.baseURL, id), v, nil)
+	// start the requests channel processor
+	go queue.processRequests(api)
+	// add the 1st request to the channel
+	queue.requestsChannel <- request
+
+	// async process the response channel
+	go (func(q *Queue, d chan *Data, e chan error, req *Request) {
+		// on done close channels
+		// close data channel
+		defer close(d)
+		// close error channel
+		defer close(e)
+
+		// listen channel
+		for {
+			// capture the response and channel state
+			res, ok := <-q.responseChannel
+			// break the loop if the channel is closed
+			if !ok {
+				break
+			}
+
+			// send the results to the data channel
+			d <- &res.Results
+			// send errors to error channel
+			if res.Error != nil {
+				e <- res.Error
+			}
+
+			// we are done! break the loop and close the channels
+			break
+		}
+	})(queue, data, errors, request)
+
+	// return the data channel
+	return data, errors
+}
